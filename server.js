dd2b8e764ac42d72ec2dd701e5e9184f0bd313cf
Jack Lee <jack200336@gmail.com>
2018-09-19 20:57:02 -0400

    guagua

---
+++ server.js
@@ -0,0 +1,7 @@
+const express = require('express')
+const app = express()
+
+app.use(express.static("static"))
+app.get('/', (req, res) => res.sendFile(__dirname + "/index.html"))
+
+app.listen(3000, () => console.log('Example app listening on port 3000!'))
